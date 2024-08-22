83c5e040c735d75382a033f119773811ef5c88e2
anndrelipe <ppeixoto.andre@gmail.com>
2024-08-22 18:43:33 -0300

    muda app listen

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,9 +1,8 @@
 import "dotenv/config";
 import app from "./src/config/app.js";
 
-const SERVER_PORT = process.env.SERVER_PORT ?? 3000;
-const SERVER_HOST = process.env.SERVER_HOST ?? "127.0.0.1";
+const SERVER_PORT = process.env.SERVER_PORT ?? "3000";
 
-app.listen(SERVER_PORT, SERVER_HOST, () => {
-    console.log(`Server listening on http://${SERVER_HOST}:${SERVER_PORT}`);
+app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port ${SERVER_PORT}`);
 });
